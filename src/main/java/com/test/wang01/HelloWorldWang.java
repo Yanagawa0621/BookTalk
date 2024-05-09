92a15c51fa1e5cdc302e0befa9754a4e3f4cfa2c
wang <tpshherman123@yahoo.com.tw>
2024-05-09 10:20:09 +0800

    分支上傳

--- src/main/java/com/test/wang01/HelloWorldWang.java
+++ src/main/java/com/test/wang01/HelloWorldWang.java
@@ -2,6 +2,6 @@ package com.test.wang01;
 
 public class HelloWorldWang {
 	public static void main(String[] args) {
-		System.out.println("HelloWorld!!WangTest");
+		System.out.println("HelloWorld!!WangTest2");
 	}
 }
