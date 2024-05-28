dbd84ecaaecae5dbdd13b529fd102ec0c4422a6d
lee <toby87.222@gmail.com>
2024-05-28 13:50:08 +0800

    add popular article page add article page add ckEditor update articleDAO update forumDAO update commentDAO

--- src/main/java/com/comment/model/CommentDAO.java
+++ src/main/java/com/comment/model/CommentDAO.java
@@ -3,86 +3,52 @@ package com.comment.model;
 import java.util.List;
 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
-
-import com.article.model.ArticleVO;
 
 import util.HibernateUtil;
 
-public class CommentDAO implements CommentDAO_interface{
+public class CommentDAO implements CommentDAO_interface {
+	private SessionFactory factory;
+
+	public CommentDAO() {
+		factory = HibernateUtil.getSessionFactory();
+	}
+
+	private Session getSession() {
+		return factory.getCurrentSession();
+	}
 
 	@Override
 	public void insert(CommentVO commentVO) {
-		// TODO Auto-generated method stub
-		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-		try {
-			session.beginTransaction();
-			session.save(commentVO);
-			session.getTransaction().commit();
 
-		} catch (Exception e) {
-			e.printStackTrace();
-			session.getTransaction().rollback();
-		}
-		
+		getSession().save(commentVO);
 	}
 
 	@Override
 	public void update(CommentVO commentVO) {
-		// TODO Auto-generated method stub
-		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-		try {
-			session.beginTransaction();
-			session.update(commentVO);
-			session.getTransaction().commit();
 
-		} catch (Exception e) {
-			e.printStackTrace();
-			session.getTransaction().rollback();
-		}
-		
+		getSession().update(commentVO);
 	}
 
 	@Override
 	public boolean updateCommentStatus(Integer commentNumber, Integer commentState) {
-		// TODO Auto-generated method stub
-		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-		try {
-            session.beginTransaction();
-            CommentVO commentVO = session.get(CommentVO.class, commentNumber);
-            if (commentVO != null) {
-            	commentVO.setCommentState(commentState);
-                session.update(commentVO);
-                session.getTransaction().commit();
-                return true;
-            }
-            
-        } catch (Exception e) {
-        		e.printStackTrace();
-        		session.getTransaction().rollback();
-            }
-            return false;
-		
+
+		CommentVO commentVO = getSession().get(CommentVO.class, commentNumber);
+		if (commentVO != null) {
+			commentVO.setCommentState(commentState);
+			getSession().update(commentVO);
+			return true;
+		}
+		return false;
 	}
 
 	@Override
 	public List<CommentVO> findByArticleNumber(Integer articleNumber) {
-		// TODO Auto-generated method stub
-		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-		List<CommentVO> commentVO = null;
 
-		try {
-			session.beginTransaction();
-			String hql = "FROM CommentVO WHERE articleVO.articleNumber = :articleNumber";
-			Query<CommentVO> query = session.createQuery(hql, CommentVO.class);
-			query.setParameter("articleNumber", articleNumber);
-			commentVO = query.list();
-			session.getTransaction().commit();
-		} catch (Exception e) {
-			e.printStackTrace();
-			session.getTransaction().rollback();
-		} 
-		return commentVO;
+		String hql = "FROM CommentVO WHERE articleVO.articleNumber = :articleNumber";
+		Query<CommentVO> query = getSession().createQuery(hql, CommentVO.class);
+		query.setParameter("articleNumber", articleNumber);
+		return query.list();
 	}
-
 }
--- src/main/java/com/forum/model/ForumDAO.java
+++ src/main/java/com/forum/model/ForumDAO.java
@@ -1,190 +1,66 @@
 package com.forum.model;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
+
 import java.util.List;
 
-import util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import com.article.model.ArticleVO;
+
+import util.HibernateUtil;
+
 
 public class ForumDAO implements ForumDAO_interface {
+	private SessionFactory factory;
 
-	static {
-		try {
-			Class.forName(Util.DRIVER);
-		} catch (ClassNotFoundException ce) {
-			ce.printStackTrace();
-		}
+	public ForumDAO() {
+		factory = HibernateUtil.getSessionFactory();
 	}
-	private static final String INSERT_STMT = "INSERT INTO forum (name) VALUES (?)";
-	private static final String SELECT_ALL_STMT = "SELECT * FROM forum";
-	private static final String SELECT_ONE_STMT = "SELECT * FROM forum WHERE forumNumber = ?";
-	private static final String DELETE_STMT = "DELETE FROM forum WHERE forumNumber = ?";
-	private static final String UPDATE_STMT = "UPDATE forum SET name = ? WHERE forumNumber = ?";
-	private static final String SELECT_NAME_STMT = "SELECT * FROM forum WHERE name LIKE CONCAT('%', ?, '%')";
 
+	private Session getSession() {
+		return factory.getCurrentSession();
+	}
 	@Override
 	public void insert(ForumVO forumVO) {
 		// TODO Auto-generated method stub
-		Connection con = null;
-		PreparedStatement pstmt = null;
-
-		try {
-
-			con = DriverManager.getConnection(Util.URL, Util.USER, Util.PASSWORD);
-			pstmt = con.prepareStatement(INSERT_STMT);
-
-			
-			pstmt.setString(1, forumVO.getName());
-
-			pstmt.executeUpdate();
-			// Handle any driver errors
-		} catch (SQLException se) {
-			se.printStackTrace();
-			// Clean up JDBC resources
-		} finally {
-			Util.closeResources(con, pstmt, null);
-		}
-
+		getSession().save(forumVO);
 	}
 
 	@Override
 	public void update(ForumVO forumVO) {
 		// TODO Auto-generated method stub
-		Connection con = null;
-		PreparedStatement pstmt = null;
-
-		try {
-
-			con = DriverManager.getConnection(Util.URL, Util.USER, Util.PASSWORD);
-			pstmt = con.prepareStatement(UPDATE_STMT);
-
-			pstmt.setString(1, forumVO.getName());
-			pstmt.setInt(2, forumVO.getForumNumber());
-			pstmt.executeUpdate();
-
-			// Handle any driver errors
-		} catch (SQLException se) {
-			se.printStackTrace();
-			// Clean up JDBC resources
-		} finally {
-			Util.closeResources(con, pstmt, null);
-		}
-
+		getSession().update(forumVO);
 	}
 
-	@Override
-	public void delete(Integer forumNumber) {
-		// TODO Auto-generated method stub
-		Connection con = null;
-		PreparedStatement pstmt = null;
-
-		try {
-
-			con = DriverManager.getConnection(Util.URL, Util.USER, Util.PASSWORD);
-			pstmt = con.prepareStatement(DELETE_STMT);
-
-			pstmt.setInt(1, forumNumber);
-			pstmt.executeUpdate();
-
-			// Handle any driver errors
-		} catch (SQLException se) {
-			se.printStackTrace();
-			// Clean up JDBC resources
-		} finally {
-			Util.closeResources(con, pstmt, null);
-		}
-	}
 
 	@Override
 	public ForumVO findByForumNumber(Integer forumNumber) {
 		// TODO Auto-generated method stub
-		ForumVO forumVO = null;
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-
-		try {
-
-			con = DriverManager.getConnection(Util.URL, Util.USER, Util.PASSWORD);
-			pstmt = con.prepareStatement(SELECT_ONE_STMT);
-			pstmt.setInt(1, forumNumber);
-			rs = pstmt.executeQuery();
-			if (rs.next()) {
-				forumVO = new ForumVO();
-				forumVO.setForumNumber(rs.getInt("forumNumber"));
-				forumVO.setName(rs.getString("name"));
-			}
-
-		} catch (SQLException se) {
-			se.printStackTrace();
-			// Clean up JDBC resources
-		} finally {
-			Util.closeResources(con, pstmt, rs);
-		}
-		return forumVO;
+		String hql = "FROM ForumVO WHERE forumNumber = :forumNumber";
+        Query<ForumVO> query = getSession().createQuery(hql, ForumVO.class);
+        query.setParameter("forumNumber", forumNumber);
+        return query.uniqueResult();
 	}
 
 	@Override
 	public List<ForumVO> getAll() {
 		// TODO Auto-generated method stub
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		List<ForumVO> forumList = new ArrayList<>();
-		ForumVO forumVO = null;
-		try {
-			con = DriverManager.getConnection(Util.URL, Util.USER, Util.PASSWORD);
-			pstmt = con.prepareStatement(SELECT_ALL_STMT);
-
-			rs = pstmt.executeQuery();
-			while (rs.next()) {
-				forumVO = new ForumVO();
-				forumVO.setForumNumber(rs.getInt("forumNumber"));
-				forumVO.setName(rs.getString("name"));
-				forumList.add(forumVO);
-			}
-		} catch (SQLException se) {
-			se.printStackTrace();
-
-		} finally {
-			Util.closeResources(con, pstmt, rs);
-		}
-		return forumList;
-
+		 String hql = "FROM ForumVO";
+	        Query<ForumVO> query = getSession().createQuery(hql, ForumVO.class);
+	        return query.list();
 	}
 
 	@Override
 	public List<ForumVO> findByForumName(String name) {
 		// TODO Auto-generated method stub
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		List<ForumVO> forumList = new ArrayList<>();
-		ForumVO forumVO = null;
-		try {
-			con = DriverManager.getConnection(Util.URL, Util.USER, Util.PASSWORD);
-			pstmt = con.prepareStatement(SELECT_NAME_STMT);
-			System.out.println(name);
-			pstmt.setString(1, name);
-			rs = pstmt.executeQuery();
-			while (rs.next()) {
-				forumVO = new ForumVO();
-				forumVO.setForumNumber(rs.getInt("forumNumber"));
-				forumVO.setName(rs.getString("name"));
-				forumList.add(forumVO);
-			}
-		} catch (SQLException se) {
-			se.printStackTrace();
-
-		} finally {
-			Util.closeResources(con, pstmt, rs);
-		}
-		return forumList;
+		 String hql = "FROM ForumVO WHERE name = :name";
+	        Query<ForumVO> query = getSession().createQuery(hql, ForumVO.class);
+	        query.setParameter("name", name);
+	        return query.list();
 	}
+
 
 //	public static void main(String[] args) {
 ////
