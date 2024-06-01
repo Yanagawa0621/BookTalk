bcf5aae134119590e0a78163582652edec1b74c4
Yanagawa0621 <158709652+Yanagawa0621@users.noreply.github.com>
2024-06-02 00:36:56 +0800

    後台新增:新增作者,修改作者,查詢作者,新增出版社,查詢出版社,修改出版社 後台修改:不再顯示書籍類別包含有多少多少書籍,修改資料庫的假資料及出版社電話號碼最大長度為17

--- src/main/java/com/bookclass/controller/BookClassServlet.java
+++ src/main/java/com/bookclass/controller/BookClassServlet.java
@@ -18,136 +18,177 @@ import com.bookclass.model.BookClassService;
 import com.bookclass.model.BookClassVO;
 
 @WebServlet("/bookClass.do")
-public class BookClassServlet extends HttpServlet{
+public class BookClassServlet extends HttpServlet {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private BookClassService bcSce;
+
 	public void init() throws ServletException {
-		bcSce=new BookClassService();
+		bcSce = new BookClassService();
 	}
-	
+
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		req.setCharacterEncoding("UTF-8");
 		String action = req.getParameter("action");
-		
-		if("ClassNumberSearch".equals(action)) {
+
+		if ("ClassNumberSearch".equals(action)) {
 			// ===接受參數===
-			Integer classNumber=Integer.valueOf(req.getParameter("classNumber"));
+			Integer classNumber = Integer.valueOf(req.getParameter("classNumber"));
 			// ===查詢資料===
-			BookClassVO bcVO=bcSce.singleQueryBc(classNumber);
+			BookClassVO bcVO = bcSce.singleQueryBc(classNumber);
 			// ===轉交資料===
 			req.setAttribute("bcVO", bcVO);
-			String url="/front-end/shop2.jsp";
+			String url = "/front-end/shop2.jsp";
 			RequestDispatcher successView = req.getRequestDispatcher(url);
 			successView.forward(req, res);
+			return;
 		}
 	}
+
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		req.setCharacterEncoding("UTF-8");
 		String action = req.getParameter("action");
-		
+
 		// ===編號搜尋===
-		if("getOne_For_Display".equals(action)) {
+		if ("getOne_For_Display".equals(action)) {
 			List<String> errorMsgs = new LinkedList<String>();
 			req.setAttribute("errorMsgs", errorMsgs);
-			// ===接受參數===
+			// ---接受參數---
 			Integer classNumber = null;
 			try {
-				classNumber=Integer.valueOf(req.getParameter("classNumber"));
-			}catch (NumberFormatException e) {
+				classNumber = Integer.valueOf(req.getParameter("classNumber"));
+			} catch (NumberFormatException e) {
 				errorMsgs.add("請輸入數字");
-				RequestDispatcher failureView=req.getRequestDispatcher("/back-end/bookClass/bookClass.jsp");
+				RequestDispatcher failureView = req.getRequestDispatcher("/back-end/bookClass/bookClass.jsp");
 				failureView.forward(req, res);
 				return;
 			}
-			// ===查詢資料===
-			List<BookClassVO> list=new ArrayList<>();
-			BookClassVO bcVO= bcSce.singleQueryBcNp(classNumber);
+			// ---查詢資料---
+			List<BookClassVO> list = new ArrayList<>();
+			BookClassVO bcVO = bcSce.singleQueryBcNp(classNumber);
 //			System.out.println(bcVO);
-			if(bcVO==null) {
+			if (bcVO == null) {
 				errorMsgs.add("查無資料");
-				RequestDispatcher failureView=req.getRequestDispatcher("/back-end/bookClass/bookClass.jsp");
+				RequestDispatcher failureView = req.getRequestDispatcher("/back-end/bookClass/bookClass.jsp");
 				failureView.forward(req, res);
 				return;
 			}
 			// ===轉交資料===
 			list.add(bcVO);
 			req.setAttribute("list", list);
-			RequestDispatcher failureView=req.getRequestDispatcher("/back-end/bookClass/BookClassQuery.jsp");
+			RequestDispatcher failureView = req.getRequestDispatcher("/back-end/bookClass/BookClassQuery.jsp");
 			failureView.forward(req, res);
+			return;
 		}
-		
+
 		// ===關鍵字搜尋===
-		if("keywords".equals(action)) {
+		if ("keywords".equals(action)) {
 			List<String> errorMsgs = new LinkedList<String>();
 			req.setAttribute("errorMsgs", errorMsgs);
-			
-			// ===接受參數===
-			String keywords=req.getParameter("keywords");
-			// ===查詢資料===
-			List<BookClassVO> list=bcSce.keywordsBcNp(keywords);
-			if(list.size()==0) {
+
+			// ---接受參數---
+			String keywords = req.getParameter("keywords");
+			// ---查詢資料---
+			List<BookClassVO> list = bcSce.keywordsBcNp(keywords);
+			if (list.size() == 0) {
 				errorMsgs.add("查無資料");
-				RequestDispatcher failureView=req.getRequestDispatcher("/back-end/bookClass/bookClass.jsp");
+				RequestDispatcher failureView = req.getRequestDispatcher("/back-end/bookClass/bookClass.jsp");
 				failureView.forward(req, res);
 				return;
 			}
-			// ===轉交資料===
+			// ---轉交資料---
 			req.setAttribute("list", list);
-			RequestDispatcher failureView=req.getRequestDispatcher("/back-end/bookClass/BookClassQuery.jsp");
+			RequestDispatcher failureView = req.getRequestDispatcher("/back-end/bookClass/BookClassQuery.jsp");
 			failureView.forward(req, res);
+			return;
 		}
-		
+
 		// ===查詢單筆資料===
-		if("getOne_For_Update".equals(action)) {
-			// ===接受參數===
-			Integer classNumber=Integer.valueOf(req.getParameter("classNumber"));
-			// ===查詢資料===
-			BookClassVO bcVO=bcSce.singleQueryBcNp(classNumber);
-			// ===轉交資料===
+		if ("getOne_For_Update".equals(action)) {
+			// ---接受參數---
+			Integer classNumber = Integer.valueOf(req.getParameter("classNumber"));
+			// ---查詢資料---
+			BookClassVO bcVO = bcSce.singleQueryBcNp(classNumber);
+			// ---轉交資料---
 			req.setAttribute("bcVO", bcVO);
-			RequestDispatcher failureView=req.getRequestDispatcher("/back-end/bookClass/updateBookClass.jsp");
+			RequestDispatcher failureView = req.getRequestDispatcher("/back-end/bookClass/updateBookClass.jsp");
 			failureView.forward(req, res);
+			return;
 		}
-		
+
 		// ===修改資料===
-		if("update".equals(action)) {
+		if ("update".equals(action)) {
 			List<String> errorMsgs = new LinkedList<String>();
 			req.setAttribute("errorMsgs", errorMsgs);
-			// ===接受參數===
-			Integer classNumber=Integer.valueOf(req.getParameter("classNumber"));
-			String className=req.getParameter("className");
-			// ===提交修改===
-			int result=bcSce.updateBc(classNumber, className);
-			System.out.println(result);
-			if(result==-1) {
+			// ---接受參數---
+			Integer classNumber = Integer.valueOf(req.getParameter("classNumber"));
+			String className = req.getParameter("className");
+			// ---提交修改---
+			int result = bcSce.updateBc(classNumber, className);
+//			System.out.println(result);
+			if(result==-2) {
+				errorMsgs.add("此類別名稱已重複");
 				BookClassVO bcVO=new BookClassVO();
+				bcVO.setClassName(className);
+				req.setAttribute("bcVO", bcVO);
+				RequestDispatcher failureView=req.getRequestDispatcher("back-end/bookClass/updateBookClass.jsp");
+				failureView.forward(req, res);
+				return;
+			}
+			if (result == -1) {
+				BookClassVO bcVO = new BookClassVO();
 				errorMsgs.add("修改失敗");
 				bcVO.setClassName(className);
 				bcVO.setClassNumber(classNumber);
 				req.setAttribute("bcVO", bcVO);
-				RequestDispatcher failureView=req.getRequestDispatcher("/back-end/bookClass/updateBookClass.jsp");
+				RequestDispatcher failureView = req.getRequestDispatcher("/back-end/bookClass/updateBookClass.jsp");
 				failureView.forward(req, res);
+				return;
 			}
-			// ===修改完成===
-			if(result==1) {
-				BookClassVO bcVO=bcSce.singleQueryBcNp(classNumber);
-				System.out.println("------------------------");
-				System.out.println(bcVO==null);
-				System.out.println(bcVO.getClassNumber());
-				System.out.println(bcVO.getClassName());
-				System.out.println(bcVO.getBpVO()==null);
-				System.out.println("------------------------");
-//				System.out.println(bcVO.getBpVO().size());
-				List<BookClassVO> list=new ArrayList<>();
-				list.add(bcVO);
-				
-				req.setAttribute("list", list);
-				RequestDispatcher failureView=req.getRequestDispatcher("/back-end/bookClass/bookClass.jsp");
+			
+			// ---修改完成---
+			if (result == 1) {
+				RequestDispatcher failureView = req.getRequestDispatcher("/back-end/bookClass/bookClass.jsp");
 				failureView.forward(req, res);
+				return;
 			}
 		}
-	}	
+
+		// ===新增類別===
+		if ("insert".equals(action)) {
+			List<String> errorMsgs = new LinkedList<String>();
+			req.setAttribute("errorMsgs", errorMsgs);
+			// ---接受參數---
+			String className = req.getParameter("className");
+			// ---提交新增---
+			int result=bcSce.increaseBc(className);
+			
+			if(result==-1) {
+				errorMsgs.add("新增失敗");
+				BookClassVO bcVO=new BookClassVO();
+				bcVO.setClassName(className);
+				req.setAttribute("bcVO", bcVO);
+				RequestDispatcher failureView=req.getRequestDispatcher("back-end/bookClass/addBookClass.jsp");
+				failureView.forward(req, res);
+				return;
+			}
+			if(result==-2) {
+				errorMsgs.add("此類別名稱已重複");
+				BookClassVO bcVO=new BookClassVO();
+				bcVO.setClassName(className);
+				req.setAttribute("bcVO", bcVO);
+				RequestDispatcher failureView=req.getRequestDispatcher("back-end/bookClass/addBookClass.jsp");
+				failureView.forward(req, res);
+				return;
+			}
+			// ---轉交結果---
+			if(result==1) {
+				RequestDispatcher failureView = req.getRequestDispatcher("/back-end/bookClass/bookClass.jsp");
+				failureView.forward(req, res);
+				return;
+			}
+		}
+	}
 }
