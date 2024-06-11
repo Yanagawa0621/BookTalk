b86bcd93d617651fd531542f894ac5580d8a1599
wang <tpshherman123@yahoo.com.tw>
2024-06-11 10:21:15 +0800

    小修改

--- src/main/java/com/qa/controller/QaServlet.java
+++ src/main/java/com/qa/controller/QaServlet.java
@@ -9,8 +9,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.callcenter.model.CallCenterVO;
 import com.qa.model.QaService;
 import com.qa.model.QaVO;
 
